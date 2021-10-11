bc73f44c73dda6b1272aad73c7e3bb61331a3c7b
M-Hamo <58711682+M-Hamo@users.noreply.github.com>
2021-10-12 01:52:18 +0200

    Preloader 🚀✌

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
-import { Component, OnInit } from "@angular/core";
+import { AfterViewInit, Component, OnInit } from "@angular/core";
 import * as AOS from "aos";
 
 @Component({
   selector: "app-root",
   template: `<router-outlet></router-outlet>`,
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
   ngOnInit(): void {
     AOS.init();
+  }
+
+  // //preload Three-dots
+  ngAfterViewInit(): void {
+    this._hidePreloader();
+  }
+
+  private _hidePreloader(): void {
+    const el = document.getElementById("globalLoader");
+    if (el) {
+      el.addEventListener("transitionend", () => {
+        el.style.display = "none";
+      });
+
+      if (!el.className.includes("global-loader-hidden")) {
+        el.style.display = "none";
+      }
+    }
   }
 }

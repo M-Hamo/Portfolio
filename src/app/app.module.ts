3c83b31bd3d7060f53fd5400bdd2e27d42496429
Mohamed Eldeeb <58711682+M-Hamo@users.noreply.github.com>
2022-03-05 04:26:50 +0200

    Revert "PWA"
    This reverts commit 50e38267be584dfeea574da31926fb2d6f949921.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,10 @@ import { SharedModule } from "src/shared/shared.module";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [...AppRoutingModule.Components],
-  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, SharedModule, ServiceWorkerModule.register('ngsw-worker.js', {
-  enabled: environment.production,
-  // Register the ServiceWorker as soon as the app is stable
-  // or after 30 seconds (whichever comes first).
-  registrationStrategy: 'registerWhenStable:30000'
-})],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
